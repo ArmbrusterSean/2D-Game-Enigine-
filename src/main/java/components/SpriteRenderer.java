9a0c38a7a831e32217777286e424c12a154738cd
ArmbrusterSean <75600452+ArmbrusterSean@users.noreply.github.com>
2021-07-27 19:41:20 -0400

    - Need to re-couple obj1 for testing purposes. - Used Java's reflection library to find and expose every desired variable for ImGui Window. - Gson able to load and save values.

--- src/main/java/components/SpriteRenderer.java
+++ src/main/java/components/SpriteRenderer.java
@@ -9,12 +9,11 @@ import renderer.Texture;
 
 public class SpriteRenderer extends Component {
 
-    private Vector4f color = new Vector4f(1,1,1,1);
+    private Vector4f color = new Vector4f(1, 1, 1, 1);
     private Sprite sprite = new Sprite();
 
-    // transient because GSON will not serialize
     private transient Transform lastTransform;
-    private transient boolean isDirty = false;
+    private transient boolean isDirty = true;
 
 //    public SpriteRenderer(Vector4f color) {
 //        this.color = color;
@@ -39,48 +38,46 @@ public class SpriteRenderer extends Component {
             this.gameObject.transform.copy(this.lastTransform);
             isDirty = true;
         }
-     }
+    }
 
     @Override
     public void imgui() {
         float[] imColor = {color.x, color.y, color.z, color.w};
-        if(ImGui.colorPicker4("Color Picker: ", imColor)) {
+        if (ImGui.colorPicker4("Color Picker: ", imColor)) {
             this.color.set(imColor[0], imColor[1], imColor[2], imColor[3]);
             this.isDirty = true;
         }
-
     }
 
-     public Vector4f getColor() {
+    public Vector4f getColor() {
         return this.color;
-     }
+    }
 
-     public Texture getTexture() {
+    public Texture getTexture() {
         return sprite.getTexture();
-     }
+    }
 
-     public Vector2f[] getTexCoords() {
+    public Vector2f[] getTexCoords() {
         return sprite.getTexCoords();
-     }
+    }
 
-     public void setSprite(Sprite sprite) {
+    public void setSprite(Sprite sprite) {
         this.sprite = sprite;
         this.isDirty = true;
-     }
+    }
 
-     public void setColor(Vector4f color) {
+    public void setColor(Vector4f color) {
         if (!this.color.equals(color)) {
             this.isDirty = true;
             this.color.set(color);
         }
-     }
+    }
 
-     public boolean isDirty() {
+    public boolean isDirty() {
         return this.isDirty;
-     }
+    }
 
-     public void setClean() {
+    public void setClean() {
         this.isDirty = false;
-     }
-
+    }
 }
--- src/main/java/jade/LevelEditorScene.java
+++ src/main/java/jade/LevelEditorScene.java
@@ -2,76 +2,82 @@ package jade;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import components.RigidBody;
 import components.Sprite;
 import components.SpriteRenderer;
 import components.SpriteSheet;
 import imgui.ImGui;
 import org.joml.Vector2f;
+import org.joml.Vector3f;
 import org.joml.Vector4f;
-import org.lwjgl.system.CallbackI;
 import util.AssetPool;
 
-import java.util.Vector;
+import static org.lwjgl.glfw.GLFW.*;
 
 public class LevelEditorScene extends Scene {
 
     private GameObject obj1;
     private SpriteSheet sprites;
-    private SpriteRenderer obj1Sprite;
+    SpriteRenderer obj1Sprite;
 
     public LevelEditorScene() {
 
     }
+
     @Override
     public void init() {
         loadResources();
         this.camera = new Camera(new Vector2f(-250, 0));
         if (loadedLevel) {
+            this.activeGameObject = gameObjects.get(0);
             return;
         }
 
         sprites = AssetPool.getSpriteSheet("assets/images/spritesheet.png");
 
-        obj1 = new GameObject("Object 1",
-                new Transform(new Vector2f(200, 100), new Vector2f(256, 256)), 2);
+        obj1 = new GameObject("Object 1", new Transform(new Vector2f(200, 100),
+                new Vector2f(256, 256)), 2);
         obj1Sprite = new SpriteRenderer();
         obj1Sprite.setColor(new Vector4f(1, 0, 0, 1));
         obj1.addComponent(obj1Sprite);
+        obj1.addComponent(new RigidBody());
         this.addGameObjectToScene(obj1);
         this.activeGameObject = obj1;
 
         GameObject obj2 = new GameObject("Object 2",
-                new Transform(new Vector2f(400, 100), new Vector2f(256, 256)),1);
+                new Transform(new Vector2f(400, 100), new Vector2f(256, 256)), 3);
         SpriteRenderer obj2SpriteRenderer = new SpriteRenderer();
         Sprite obj2Sprite = new Sprite();
         obj2Sprite.setTexture(AssetPool.getTexture("assets/images/blendImage2.png"));
         obj2SpriteRenderer.setSprite(obj2Sprite);
         obj2.addComponent(obj2SpriteRenderer);
         this.addGameObjectToScene(obj2);
-
     }
 
-    // load resources at loading time
     private void loadResources() {
         AssetPool.getShader("assets/shaders/default.glsl");
 
+        // TODO: FIX TEXTURE SAVE SYSTEM TO USE PATH INSTEAD OF ID
         AssetPool.addSpriteSheet("assets/images/spritesheet.png",
                 new SpriteSheet(AssetPool.getTexture("assets/images/spritesheet.png"),
                         16, 16, 26, 0));
+        AssetPool.getTexture("assets/images/blendImage2.png");
     }
 
     @Override
     public void update(float dt) {
+
         for (GameObject go : this.gameObjects) {
             go.update(dt);
         }
+
         this.renderer.render();
     }
 
     @Override
     public void imgui() {
-        ImGui.begin("Test Window");
-        ImGui.text("SOme Text! :P");
+        ImGui.begin("Test window");
+        ImGui.text("Some random text");
         ImGui.end();
     }
 }
